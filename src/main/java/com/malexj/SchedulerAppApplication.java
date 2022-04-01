c2ecf3043eb1b51b4c8d7a2961475d214a5f7d4a
malexj <alexmaximovj@gmail.com>
2022-04-02 00:56:21 +0300

    Add zipking configuration

--- src/main/java/com/malexj/SchedulerAppApplication.java
+++ src/main/java/com/malexj/SchedulerAppApplication.java
@@ -3,10 +3,8 @@ package com.malexj;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling
 @EnableAsync
 public class SchedulerAppApplication {
 
